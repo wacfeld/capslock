070ca47cde04a27fc7d73fa707ae33bcd131ff79
wacfeld <wacfeldwang333@gmail.com>
2022-01-17 12:40:26 -0500

    finished flowchart converter

---
+++ flowchart.py
@@ -0,0 +1,98 @@
+#!/bin/env python3
+
+import sys
+
+# two flowchart formats:
+# - dot (theirs)
+# - mini (mine)
+
+# questions have multiple named outputs
+# commands have a single unnamed output
+# each file starts with the name on one line
+# followed by names of all things
+# things ending in question marks are questions
+# followed by their connections, one per line
+# things are space separated
+# all sections are separated by a space
+# ex.
+'''
+collatz
+
+0 even?
+1 divide by two
+2 multiply by three, add one
+
+0 yes 1
+0 no 2
+1 0
+2 0
+'''
+
+# see if string ends in '?'
+def isquestion(s):
+    if s[-1] == '?':
+        return 'q'
+    else:
+        return 'c'
+
+# delete all blanks at front of list
+def skipblanks(l):
+    while l[0] == '':
+        del l[0]
+
+# not gonna implement anytime soon, hard
+# def dot2mini():
+
+# questions are ovals
+# commands are rectangles
+def mini2dot(minifile):
+    dotfile = "digraph G {\n"
+
+    minifile = minifile.split('\n')
+
+    skipblanks(minifile)
+    title = minifile[0]
+    del minifile[0] # keep moving forward
+
+    skipblanks(minifile)
+    nametypes = {} # dictionary of name types (question/command)
+    while(minifile[0]) != '': # assign names
+        pair = minifile[0].split(maxsplit=1) # split on first space
+        nametypes[pair[0]] = isquestion(pair[1]) # store if it is a question
+        dotfile += pair[0] + '[label="' + pair[1] + '"'
+        if nametypes[pair[0]] == 'c': # command
+            dotfile += ',shape=box'
+
+        dotfile += '];\n'
+
+        del minifile[0] # keep moving forward
+    
+    skipblanks(minifile)
+
+    # connections
+    while len(minifile) > 0 and minifile[0] != '':
+        cur = minifile[0]
+        src = cur.split(maxsplit=1)[0]
+        mid = None
+        dest = cur[::-1].split(maxsplit=1)[0] # reverse string, split
+
+        dotfile += src + ' -> ' + dest
+        if nametypes[src] == 'q': # question
+            mid = cur.split(maxsplit=1)[1][::-1].split(maxsplit=1)[1][::-1] # cut off src, reverse, cut off dest, reverse
+            dotfile += '[label="' + mid + '"]'
+        
+        dotfile += ";\n"
+
+        del minifile[0] # tatakae
+    
+    # add title
+    dotfile += 'labelloc="t";\n'
+    dotfile += 'label=' + title + ';\n'
+    
+    dotfile += '}' # close
+
+    return dotfile
+
+
+minifile = sys.stdin.read()
+print(mini2dot(minifile))
